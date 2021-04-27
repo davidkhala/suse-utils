940b4da6169490c51d9c45eb29975fea40b221f6
David Liu <david.yx.liu@oracle.com>
2021-04-27 04:09:38 +0000

    force install

--- SLES/12/mysql.sh
+++ SLES/12/mysql.sh
@@ -4,14 +4,17 @@ install8() {
     cd /etc/zypp/repos.d/
     wget https://download.opensuse.org/repositories/devel:/gcc/SLE-12/devel:gcc.repo
     cd -
-    sudo rpm -Uvh ./mysql80-community-release-sles12-3.noarch.rpm
+    sudo rpm --upgrade --force --verbose ./mysql80-community-release-sles12-3.noarch.rpm
     sudo rpm --import /etc/RPM-GPG-KEY-mysql
+    sudo zypper modifyrepo -d mysql80-community
+    sudo zypper refresh
+    sudo zypper install mysql-community-server
 }
 install5(){
     cd /etc/zypp/repos.d/
     sudo wget https://download.opensuse.org/repositories/devel:/gcc/SLE-12/devel:gcc.repo
     cd -
-    sudo rpm -Uvh ./mysql80-community-release-sles12-3.noarch.rpm
+    sudo rpm --upgrade --force --verbose ./mysql80-community-release-sles12-3.noarch.rpm
     sudo rpm --import /etc/RPM-GPG-KEY-mysql
     sudo zypper modifyrepo -d mysql80-community
     sudo zypper modifyrepo -e mysql57-community
