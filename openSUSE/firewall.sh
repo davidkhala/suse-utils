1e8b4009bfbefc4c90f0215c652ff162948cdbe9
David Liu <david.yx.liu@oracle.com>
2022-10-30 20:53:11 +0800

    Update firewall.sh

--- openSUSE/firewall.sh
+++ openSUSE/firewall.sh
@@ -1,3 +1,5 @@
+# openSUSE Leap 15.0 introduces firewalld as the new default software firewall.
+# See in https://doc.opensuse.org/documentation/leap/security/html/book-security/cha-security-firewall.html
 install(){
    sudo zypper install -y firewalld
 }
